28561b4d505a02eae1ef443d621ce8cd3397b3b7
Kwok Cheong <wongkwokcheong@hotmail.com>
2022-03-17 01:33:12 +0800

    added farmed data

--- project.r
+++ project.r
@@ -1,13 +1,12 @@
 library(ggplot2)
 library(httr)
 library(jsonlite)
+library("writexl")
 
 ######## URLS TO USE ###########
 # alternate url: https://shopee.sg/api/v4/product/get_shop_info?shopid=194330690
-
-
 orderBrush <- read.csv("order_brush_order.csv")
-shopIds <- orderBrush$shopid
+shopIds <- unique(orderBrush$shopid)
 
 # initialize the empty data frame
 df_shopId <- data.frame(matrix(ncol = 10, nrow = 0))
@@ -32,3 +31,32 @@ for(i in shopIds){
   count <- count + 1
   print(count)
 }
+
+write_xlsx(df_shopId,"C:\\Users\\wongk\\Documents\\R Studio Projects\\TBA2104_Predictive_Analytics_Project\\shopid_data.xlsx")
+
+
+#https://shopee.sg/api/v4/shop/get_categories?limit=20&offset=0&shopid=194330690
+apiCallShopCategory <- function(shop_id){
+  res = GET("https://shopee.sg/api/v4/shop/get_categories?limit=20&offset=0", query = list(shopid = shop_id))
+  data <- fromJSON(rawToChar(res$content))
+  jsonData <- data$data
+  return(jsonData)
+}
+
+df_shopCategory <- data.frame(matrix(ncol = 2, nrow = 0))
+names(df_shopId) <- c('id', 'shop_categories')
+
+
+count <- 0
+for(i in shopIds){
+  data <- apiCallShopCategory(i)
+  df <- data.frame(matrix(ncol = 2, nrow = 0))
+  colnames(df) <- c('id', 'shop_categories')
+  df <- c(i, data$shop_categories$display_name)
+  df_shopCategory <- rbind(df_shopCategory, df)
+  names(df_shopCategory) <- c('id', 'shop_categories')
+  count <- count + 1
+  print(count)
+}
+
+
