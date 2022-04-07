92dfd30ee4a3cd997f8477a07ceca1ab7f57b402
Kwok Cheong <wongkwokcheong@hotmail.com>
2022-04-07 21:47:15 +0800

    Cleaned dataset and created new xlsx as main_data_cleaned

---
+++ proj1.r
@@ -0,0 +1,18 @@
+library(dplyr)
+library(randomForest)
+library(e1071)
+library(corrplot)
+library(naivebayes)
+library(readxl)
+library(xlsx)
+
+data <- read.csv('Main_data.csv', header = TRUE, na.strings=c('NIL'))
+clean_data <- na.omit(data)
+
+# examine data
+# Check if any missing values
+sum(is.na(clean_data))
+# check data
+head(clean_data)
+
+write.xlsx(clean_data,file="main_data_cleaned.xlsx", sheetName="Order_brush",col.names = TRUE)
