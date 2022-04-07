3f83caec942c66c80f714d9ed39a7d7e45ae52ac
Kwok Cheong <wongkwokcheong@hotmail.com>
2022-04-07 22:08:44 +0800

    replaced response rate above 100 to 100

--- proj1.r
+++ proj1.r
@@ -15,4 +15,9 @@ sum(is.na(clean_data))
 # check data
 head(clean_data)
 
+replace(clean_data$Shop.Response.Rate, clean_data$Shop.Response.Rate > 100, 100)
+clean_data$Shop.Response.Rate[clean_data$Shop.Response.Rate > 100] <- 100
+max(clean_data$Shop.Response.Rate)
+
 write.xlsx(clean_data,file="main_data_cleaned.xlsx", sheetName="Order_brush",col.names = TRUE)
+
